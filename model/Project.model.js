2ec03ed4484e71c36229b19de51b041c8233486f
NAoHR <najmim625@gmail.com>
2022-03-14 15:52:16 +0700

    add project model

---
+++ model/Project.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const ProjectSchema = Schema({
+    belongsTo : {
+        type : mongoose.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    name : {
+        type : String,
+        required : true,
+        maxLength : 150
+    },
+    description : {
+        type : String,
+        required : true,
+        maxLength: 255
+    },
+    link : {
+        type : String,
+        required : true,
+        maxLength: 255
+    }
+})
+
+module.exports = mongoose.model("Project",ProjectSchema)
