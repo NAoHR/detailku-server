884406c51e10708c646c15e39df22320a43f68c1
NAoHR <najmim625@gmail.com>
2022-03-14 15:51:32 +0700

    add certificate model

---
+++ model/Cert.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const CertSchema = {
+    belongsTo : {
+        type : mongoose.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    title : {
+        type : String,
+        required : true,
+        maxLength : 150
+    },
+    organizer : {
+        type : String,
+        required : true,
+        maxLength : 150
+    },
+    certID : {
+        type : String,
+        required : true,
+    },
+    certLink : {
+        type : String
+    }
+}
+
+module.exports = mongoose.model("Cert",CertSchema)
