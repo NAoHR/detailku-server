2998a7beee0ecddf6a139812d9bcd44e661de2d4
NAoHR <najmim625@gmail.com>
2022-03-14 15:53:21 +0700

    add user model

---
+++ model/User.model.js
@@ -0,0 +1,82 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Token = require("./Token.model");
+
+require("dotenv").config();
+
+const { Schema } = mongoose;
+
+const UserSchema = Schema({
+    name : {
+        type : String,
+        required : true,
+        minLength : 3
+    },
+    password : {
+        type : String,
+        required : true,
+    },
+    grade : {
+        type : String,
+        required : true,
+        enum : [
+            "X_SIJA_1",
+            "X_SIJA_2",
+            "XI_SIJA_1",
+            "XI_SIJA_2",
+            "XII_SIJA_1",
+            "XII_SIJA_2"
+        ]
+    },
+    username : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    detail : {
+        type : Schema.Types.ObjectId,
+        ref : "Detail"
+    },
+    certificate : [
+        {
+            type : Schema.Types.ObjectId,
+            ref : "Cert"
+        }
+    ],
+    project : [
+        {
+            type : Schema.Types.ObjectId,
+            ref : "Project"
+        }
+    ],
+    skill : [{
+        type : Schema.Types.ObjectId,
+        ref : "Skill"
+    }]
+})
+
+UserSchema.methods = {
+    createAccessToken : async function (){
+        try{
+            const {_id} = this;
+            const detailkutoken = await jwt.sign({
+                "uid" : _id
+            },process.env.SECRET_AT,{
+                expiresIn : "2d"
+            });
+
+            const tokenDb  = new Token({
+                tokenId : detailkutoken
+            })
+            await tokenDb.save();
+
+            return detailkutoken;
+
+        }catch(e){
+            return false
+        }
+
+    }
+}
+
+module.exports = mongoose.model("User",UserSchema);
