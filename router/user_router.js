2d15df83737294e4ed453d1df0ce45afb3273fbb
NAoHR <najmim625@gmail.com>
2022-06-22 16:08:09 +0700

    refactor: better naming on auth

--- router/user_router.js
+++ router/user_router.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const {authPostEdit,authorizeUser} = require("../middleware/authMiddleware");
+const {authorization,authentication} = require("../middleware/authMiddleware");
 const userControler = require("../controller/userControler")
 
 // add
-router.post("/add/cert", authorizeUser, userControler.postcert);
-router.post("/add/skill", authorizeUser, userControler.postSkill);
-router.post("/add/project", authorizeUser, userControler.postProject);
+router.post("/add/cert", authentication, userControler.postcert);
+router.post("/add/skill", authentication, userControler.postSkill);
+router.post("/add/project", authentication, userControler.postProject);
 
 // get
-router.get("/me", authorizeUser, userControler.me);
-router.get("/me/skill", authorizeUser, userControler.skillGet);
-router.get("/me/project", authorizeUser, userControler.projectGet);
-router.get("/me/cert", authorizeUser, userControler.certGet);
+router.get("/me", authentication, userControler.me);
+router.get("/me/skill", authentication, userControler.skillGet);
+router.get("/me/project", authentication, userControler.projectGet);
+router.get("/me/cert", authentication, userControler.certGet);
 
 // edit 
-router.put("/edit/skill/:postId",authorizeUser, authPostEdit, userControler.editSkill)
+router.put("/edit/skill/:postId",authentication, authorization, userControler.editSkill)
 
 // delete
-router.delete("/delete/:postType/:postId", authorizeUser, authPostEdit, userControler.deletePost);
+router.delete("/delete/:postType/:postId", authentication, authorization, userControler.deletePost);
 
 
 module.exports = router;
