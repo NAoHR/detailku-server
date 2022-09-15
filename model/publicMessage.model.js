d40f03b77fe88d9a186bea5b665137c114e02eac
naohr <najmim625@gmail.com>
2022-09-15 12:34:09 +0700

    update: add anonymous message

---
+++ model/publicMessage.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const PublicMsgSChema = Schema({
+    sender : {
+        type: String,
+        default : "Anon",
+        maxLength: 100
+    },
+    message: {
+        type: String,
+        maxLength: 255,
+        required: [true, "message must be filled"]
+    }
+})
+
+module.exports = mongoose.model("PublicMsg", PublicMsgSChema)
--- router/client_router.js
+++ router/client_router.js
@@ -7,5 +7,8 @@ router.get("/people", getUrlMiddleware, clientController.alldata);
 router.get("/user/:username", clientController.usernameBasedUser);
 router.get("/grade/:grade",getUrlMiddleware ,clientController.gradeBasedUser);
 router.get("/jobs", clientController.jobData);
+router.get("/publicMsg", clientController.getPublicMsg);
+
+router.post("/post/publicMsg", clientController.postPublicMsg);
 
 module.exports = router
